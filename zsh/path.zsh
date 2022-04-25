36933128d8613e7c537075f73ec1e1453c702379
kuckhwan cho <rnrghks09@gmail.com>
2022-04-25 19:43:15 +0900

    Update path.zsh

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -2,4 +2,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$HOME/.cargo/bin:$PATH"
+export PATH="$PYENV_ROOT/bin:$HOME/.cargo/bin:${KREW_ROOT:-$HOME/.krew}:$PATH"
