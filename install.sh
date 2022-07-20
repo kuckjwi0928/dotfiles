464537767b4a5c635d281c23738a5d53211b0f74
kuckhwan cho <rnrghks09@gmail.com>
2022-07-20 15:57:26 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+brew bundle
 
 git clone https://github.com/dracula/zsh.git $ZSH/themes
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH/plugins/zsh-autosuggestions
-
-brew bundle
 
 (
   set -x; cd "$(mktemp -d)" &&
