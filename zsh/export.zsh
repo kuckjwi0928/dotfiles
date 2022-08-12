e4c34882552dffc6c1ee26b17be76a32d1f2eb79
kuckhwan cho <rnrghks09@gmail.com>
2022-08-12 13:38:15 +0900

    Update and rename path.zsh to export.zsh

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -3,3 +3,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$HOME/.cargo/bin:${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+export KUBE_EDITOR="/opt/homebrew/bin/nvim"
