1308ef987ee63bbcafddf6ec59a76b52a7a64a97
kuckhwan cho <rnrghks09@gmail.com>
2022-12-09 10:55:12 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -14,6 +14,9 @@ mv zsh-dracula/lib/async.zsh $ZSH/themes/lib/async.zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH/plugins/zsh-autosuggestions
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 (
   set -x; cd "$(mktemp -d)" &&
   OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
