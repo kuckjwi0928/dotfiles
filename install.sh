22aa0e2aa19cf0916cf628d02878944481a31bb2
kuckhwan cho <rnrghks09@gmail.com>
2023-08-21 13:16:06 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -36,6 +36,7 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 
 kubectl krew install view-secret
 kubectl krew install sniff
+kubectl krew install tree
 
 npm install -g @bchatard/alfred-jetbrains
 
