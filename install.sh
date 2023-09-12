3576d44b5f7b8229c4ce1d02218dc9ec2454a5e9
kuckhwan cho <rnrghks09@gmail.com>
2023-09-12 02:31:04 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,6 +39,7 @@ kubectl krew install sniff
 kubectl krew install tree
 kubectl krew install access-matrix
 
+nvm install --lts
 npm install -g @bchatard/alfred-jetbrains
 
 mkdir -p $HOME/.config && cp -R ./nvim $HOME/.config
