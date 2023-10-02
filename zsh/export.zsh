c35154113bf4aa170899fc97b23e0cb616f949be
kuckhwan cho <rnrghks09@gmail.com>
2023-10-02 15:21:58 +0900

    Update export.zsh

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -4,7 +4,6 @@ export NVM_DIR="$HOME/.nvm"
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$HOME/.cargo/bin:${KREW_ROOT:-$HOME/.krew}/bin:$(go env GOPATH)/bin:$PATH"
 export KUBE_EDITOR="/opt/homebrew/bin/nvim"
-export DRACULA_DISPLAY_FULL_CWD=1
 export EDGE_PORT=31566
 export LANG="en_US.UTF-8"
 export ZSH="$HOME/.oh-my-zsh"
