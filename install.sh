1f2143f8cdee9769511e0a26f310b4ffcaf25273
kuckjwi <rnrghks09@gmail.com>
2024-01-30 13:18:44 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 /bin/bash -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+/bin/bash -c "curl -sSL https://get.rvm.io | bash -s stable --ruby"
 
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
 
