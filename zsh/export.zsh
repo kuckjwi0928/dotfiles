7fa56d2e7bf02500e27f52d6e5a92875b64ae83f
kuckjwi <rnrghks09@gmail.com>
2024-02-26 10:36:08 +0900

    Update export.zsh

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -2,9 +2,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
 [ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$HOME/.cargo/bin:${KREW_ROOT:-$HOME/.krew}/bin:$(go env GOPATH)/bin:$PATH"
 export KUBE_EDITOR="/opt/homebrew/bin/nvim"
 export EDGE_PORT=31566
 export LANG="en_US.UTF-8"
 export ZSH="$HOME/.oh-my-zsh"
-export PATH="$PATH:$HOME/.rvm/bin"
+export PATH="$PYENV_ROOT/bin:$HOME/.cargo/bin:${KREW_ROOT:-$HOME/.krew}/bin:$(go env GOPATH)/bin:$HOME/.rvm/bin:$PATH"
