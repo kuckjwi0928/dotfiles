1676716af1470baee77e1ef1fd90582bcd34a16e
kuckjwi <rnrghks09@gmail.com>
2024-03-08 18:47:09 +0900

    Update export.zsh

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -1,6 +1,4 @@
 export NVM_DIR="$HOME/.nvm"
-[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
-[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
 export PYENV_ROOT="$HOME/.pyenv"
 export KUBE_EDITOR="/opt/homebrew/bin/nvim"
 export ZSH="$HOME/.oh-my-zsh"
