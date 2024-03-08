2c55a22cdda7fd50c227b067bba0d60c930e00ef
kuckjwi <rnrghks09@gmail.com>
2024-03-08 18:47:30 +0900

    Update setup.zsh

--- zsh/setup.zsh
+++ zsh/setup.zsh
@@ -2,4 +2,7 @@ eval "$(pyenv init --path)"
 eval "$(pyenv virtualenv-init -)"
 eval "$(fasd --init auto)"
 
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"
+
 source ~/.p10k.zsh
