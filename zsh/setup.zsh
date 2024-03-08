a2a9102ff22fa861efd6d628eb7535f6942e0360
kuckjwi <rnrghks09@gmail.com>
2024-03-08 18:57:23 +0900

    Update setup.zsh

--- zsh/setup.zsh
+++ zsh/setup.zsh
@@ -3,5 +3,4 @@ eval "$(pyenv virtualenv-init -)"
 eval "$(fasd --init auto)"
 
 [ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"
-
-source ~/.p10k.zsh
+[ -s "$HOME/.p10k.zsh" ] && \. "$HOME/.p10k.zsh"
