da79ae9ab89706f96c52ea26e120ccb8f18fbcc7
kuckjwi <rnrghks09@gmail.com>
2024-03-23 22:09:40 +0900

    Update export.zsh

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -3,4 +3,3 @@ export PYENV_ROOT="$HOME/.pyenv"
 export KUBE_EDITOR="/opt/homebrew/bin/nvim"
 export ZSH="$HOME/.oh-my-zsh"
 export PATH="$PYENV_ROOT/bin:$HOME/.cargo/bin:${KREW_ROOT:-$HOME/.krew}/bin:$(go env GOPATH)/bin:$HOME/.rvm/bin:$PATH"
-export KIND_EXPERIMENTAL_PROVIDER=podman
