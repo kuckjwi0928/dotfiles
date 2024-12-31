2ec8e9a4b31978306fd477d824f607421ab66c5b
kuckjwi <rnrghks09@gmail.com>
2024-12-31 16:05:08 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 /bin/bash -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 /bin/bash -c "$(curl -sSL https://get.rvm.io | bash -s stable --ruby)"
-/bin/bash -c "$(curl -L -o pkl https://github.com/apple/pkl/releases/download/0.25.1/pkl-linux-aarch64 chmod +x pkl)"
-
 /bin/bash -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1
@@ -13,17 +11,15 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 brew bundle
 
-curl -fsSL https://raw.githubusercontent.com/yusufcanb/tlm/release/1.1/install.sh | sudo bash -E
-
-git config --global core.editor "nvim"
-git config --global core.pager "delta"
-git config --global interactive.diffFilter "delta --color-only"
-git config --global delta.navigate "true"
-git config --global delta.line-numbers "true"
-git config --global delta.hyperlinks "true"
-git config --global delta.side-by-side "true"
-git config --global merge.conflictstyle "diff3"
-git config --global diff.colorMoved "default"
+git config --global core.editor "nvim" && \
+  git config --global core.pager "delta" && \
+  git config --global interactive.diffFilter "delta --color-only" && \
+  git config --global delta.navigate "true" && \
+  git config --global delta.line-numbers "true" && \
+  git config --global delta.hyperlinks "true" && \
+  git config --global delta.side-by-side "true" && \
+  git config --global merge.conflictstyle "diff3" && \
+  git config --global diff.colorMoved "default"
 
 (
   set -x; cd "$(mktemp -d)" &&
@@ -35,26 +31,23 @@ git config --global diff.colorMoved "default"
   ./"${KREW}" install krew
 )
 
-kubectl krew install view-secret
-kubectl krew install sniff
-kubectl krew install tree
-kubectl krew install access-matrix
-kubectl krew install stern
-kubectl krew install resource-capacity
+kubectl krew install view-secret && \ 
+  kubectl krew install sniff && \
+  kubectl krew install tree && \
+  kubectl krew install access-matrix
+  kubectl krew install stern && \
+  kubectl krew install resource-capacity
 
-mkdir -p $HOME/.config && cp -R ./nvim $HOME/.config
-mkdir -p $HOME/.aws && cp -R ./aws $HOME/.aws
-cp -R ./zsh .p10k.zsh .zshrc .curlrc $HOME
+mkdir -p $HOME/.config && cp -R ./nvim $HOME/.config && \
+  mkdir -p $HOME/.aws && cp -R ./aws $HOME/.aws && \
+  cp -R ./zsh .p10k.zsh .zshrc .curlrc $HOME
 
-kubectl completion zsh > $HOME/.local/share/zap/completion/_kubectl
-helm completion zsh > $HOME/.local/share/zap/completion/_helm
-kaf completion zsh > $HOME/.local/share/zap/completion/_kaf
-ln -s /opt/homebrew/opt/nvm/etc/bash_completion.d/nvm $HOME/.local/share/zap/completion/_nvm
+kubectl completion zsh > $HOME/.local/share/zap/completion/_kubectl && \
+  helm completion zsh > $HOME/.local/share/zap/completion/_helm && \
+  kaf completion zsh > $HOME/.local/share/zap/completion/_kaf && \
+  ln -s /opt/homebrew/opt/nvm/etc/bash_completion.d/nvm $HOME/.local/share/zap/completion/_nvm
 
 source ~/.nvm/nvm.sh
 
 nvm install --lts
 npm install -g @bchatard/alfred-jetbrains
-
-chmod +x pkl
-sudo mv pkl /usr/local/bin
