b11c18b3cf2f9837a047d9fc0b10f6af0d98ea57
kuckjwi <rnrghks09@gmail.com>
2025-11-18 21:37:36 +0900

    Change KUBE_EDITOR to use 'cursor -w'

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -1,5 +1,5 @@
 export NVM_DIR="$HOME/.nvm"
 export PYENV_ROOT="$HOME/.pyenv"
-export KUBE_EDITOR="/opt/homebrew/bin/nvim"
+export KUBE_EDITOR="cursor -w"
 export ZSH="$HOME/.oh-my-zsh"
 export PATH="$PYENV_ROOT/bin:$HOME/.cargo/bin:${KREW_ROOT:-$HOME/.krew}/bin:$(go env GOPATH)/bin:$HOME/.rvm/bin:$PATH"
